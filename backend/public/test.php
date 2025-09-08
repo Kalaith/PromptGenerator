872a710cc4c3d170d5d988d2c2f514f902ef2964
Kalaith <daniel.allan@wurth.com.au>
2025-09-08 20:45:18 +1000

    cleanup

--- backend/public/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "Backend is working! API endpoint test.";
-echo "<br>";
-echo "Request URI: " . $_SERVER['REQUEST_URI'];
-echo "<br>";
-echo "Script Name: " . $_SERVER['SCRIPT_NAME'];
-echo "<br>";
-echo "Query String: " . $_SERVER['QUERY_STRING'];
-?>
